9825887503a1486dd064312c4bc57a14f59a16bb
NclFrei <nicollasfreieleoterio@gmail.com>
2025-05-15 22:41:45 -0300

    Adicionar arquivos de projeto.

---
+++ MottuChallenge.Infrastructure/Data/MottuChallengeContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MottuChallenge.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MottuChallenge.Infrastructure.Data;
+
+public class MottuChallengeContext : DbContext
+{
+    public MottuChallengeContext(DbContextOptions<MottuChallengeContext> options) : base(options)
+    {
+    
+    }
+
+    public DbSet<User> User { get; set; }
+    
+}
